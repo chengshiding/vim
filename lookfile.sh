451245412af26244ff939119a34df1113a5e9fce
RITU_chengshiding <xxx@cvte.com>
2016-05-11 11:59:26 +0800

    [feature][][] update
    [what]
    [why]
    [how]

--- lookfile.sh
+++
@@ -1,8 +0,0 @@
-#/bin/sh
-string=filenametags
-echo -e "!_TAG_FILE_SORTED\t2\t/2=foldcase/" > $string
-
-find . -regex '.*\.\(c\|C\|h\|H\|cpp\|hpp\|cxx\|hxx\|sh\|xml\|inl\)' ! \( -path "*svn*" -o -path "*repo*" -o -path "*git*" \) -type f -printf "%f\t%p\t1\n" | sort -f >> $string
-
-pwd=`pwd`
-sed -i  "s+\.\/+$pwd\/+g" $string
